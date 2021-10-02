7d6658921c8c6110964a39da162ea199ece177fc
Stefan Arentz <stefan@arentz.ca>
2021-10-02 08:58:59 -0400

    Initial Commit

---
+++ WhatIsMyIP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatIsMyIP
+//
+//  Created by Stefan Arentz on 02/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatIsMyIP/WhatIsMyIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatIsMyIPApp.swift
+//  WhatIsMyIP
+//
+//  Created by Stefan Arentz on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct WhatIsMyIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
